7902a57ffb9d840d24343d2f0d62ec3680543293
Aleksandr Pozdnikin <chishirik0@gmail.com>
2022-02-20 17:56:48 +0300

    feat: add UIAlertController

--- Netology_IB_Instruments/InfoViewController.swift
+++ Netology_IB_Instruments/InfoViewController.swift
@@ -12,19 +12,24 @@ class InfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .green
-
-        // Do any additional setup after loading the view.
+        
+        let button = UIButton(frame: CGRect(x: 50, y: 300, width: 200, height: 50))
+        button.setTitle("press me", for: .normal)
+        button.backgroundColor = .black
+        button.addTarget(self, action: #selector(tap), for: .touchUpInside)
+        view.addSubview(button)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func tap(){
+        let alertVC = UIAlertController(title: "Error", message: "Fatal Error", preferredStyle: .alert)
+        let actionOk = UIAlertAction(title: "OK", style: .cancel) { actionOk in
+            print("Tap Ok")
+        }
+        let actionCancel = UIAlertAction(title: "Cancel", style: .destructive) { actionCancel in
+            print("Tap Cancel")
+        }
+        alertVC.addAction(actionOk)
+        alertVC.addAction(actionCancel)
+        self.present(alertVC, animated: true, completion: nil)
     }
-    */
 
 }
