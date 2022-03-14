35f1dcfbab96476607e715a79e42b38c0c469dba
Planner0 <chishirik0@gmail.com>
2022-03-15 00:26:19 +0300

    feat: change ProfileHeaderView and ProfileViewController

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -16,10 +16,23 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
         self.view.addSubview(profileHeaderView)
+        self.profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        [
+            self.profileHeaderView.topAnchor.constraint(
+                equalTo: self.view.safeAreaLayoutGuide.topAnchor
+            ),
+            self.profileHeaderView.leadingAnchor.constraint(
+                equalTo: self.view.leadingAnchor
+            ),
+            self.profileHeaderView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor
+            ),
+            self.profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+        ]
+            .forEach{ $0.isActive = true }
         
     }
     override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        profileHeaderView.frame = self.view.frame
+//        super.viewWillLayoutSubviews()
+//        profileHeaderView.frame = self.view.frame
     }
 }
