1ebeb9becd4c0e5901e4b22c2577bf00928e1f58
Planner0 <chishirik0@gmail.com>
2022-04-27 21:25:52 +0300

    feat: add SnapKit and iOSIntPackage

--- Netology_IB_Instruments/Profile/PostTableViewCell.swift
+++ Netology_IB_Instruments/Profile/PostTableViewCell.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import SnapKit
+import iOSIntPackage
 
 class PostTableViewCell: UITableViewCell {
+    private lazy var imageProcessor = ImageProcessor()
     let authorLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .black
         label.font = .systemFont(ofSize: 20, weight: .bold)
         return label
@@ -24,7 +26,6 @@ class PostTableViewCell: UITableViewCell {
     }()
     let postTextView: UILabel = {
         let textView = UILabel()
-        textView.translatesAutoresizingMaskIntoConstraints = false
         textView.font = .systemFont(ofSize: 15, weight: .regular)
         textView.isEnabled = false
         textView.lineBreakStrategy = .hangulWordPriority
@@ -34,14 +35,12 @@ class PostTableViewCell: UITableViewCell {
     }()
     let likesLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .black
         label.font = .systemFont(ofSize: 15, weight: .regular)
         return label
     }()
     let viewsLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .black
         label.font = .systemFont(ofSize: 15, weight: .regular)
         return label
@@ -54,30 +53,36 @@ class PostTableViewCell: UITableViewCell {
         self.separatorInset.left = 15
     }
     func constraints(){
-        NSLayoutConstraint.activate([
-            self.viewsLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -5),
-            self.viewsLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -20),
-            self.viewsLabel.heightAnchor.constraint(equalToConstant: 15),
-            
-            self.authorLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 16),
-            self.authorLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 5),
-            self.authorLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -16),
-            self.authorLabel.heightAnchor.constraint(equalToConstant: 25),
-            
-            self.likesLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 20),
-            self.likesLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -5),
-            self.likesLabel.heightAnchor.constraint(equalToConstant: 15),
-            
-            self.postTextView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 16),
-            self.postTextView.topAnchor.constraint(equalTo: self.postImageView.bottomAnchor, constant: 5),
-            self.postTextView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -16),
-            self.postTextView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -25),
-            
-            self.postImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
-            self.postImageView.topAnchor.constraint(equalTo: self.authorLabel.bottomAnchor, constant: 5),
-            self.postImageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
-            self.postImageView.heightAnchor.constraint(equalToConstant: 230)
-        ])
+        
+        self.viewsLabel.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(5)
+            make.right.equalToSuperview().inset(20)
+            make.height.equalTo(15)
+        }
+        self.authorLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(16)
+            make.top.equalToSuperview().inset(5)
+            make.right.equalToSuperview().inset(16)
+            make.height.equalTo(25)
+        }
+        self.likesLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(5)
+            make.height.equalTo(15)
+        }
+
+        self.postImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.top.equalTo(self.authorLabel.snp.bottom).inset(-5)
+            make.right.equalToSuperview()
+            make.height.equalTo(230)
+        }
+        self.postTextView.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(16)
+            make.top.equalTo(self.postImageView.snp.bottom).inset(-5)
+            make.right.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(25)
+        }
     }
     func addSubview(){
         self.contentView.addSubview(viewsLabel)
@@ -87,7 +92,11 @@ class PostTableViewCell: UITableViewCell {
         self.contentView.addSubview(likesLabel)
     }
     func setupCell(image: String) {
-            postImageView.image = UIImage(named: image)
+        guard let filter = ColorFilter.allCases.randomElement() else {return}
+        guard let inputImage = UIImage(named: image) else {return}
+        imageProcessor.processImage(sourceImage: inputImage, filter: filter) { outputImage in
+            postImageView.image = outputImage
+        }
         }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
