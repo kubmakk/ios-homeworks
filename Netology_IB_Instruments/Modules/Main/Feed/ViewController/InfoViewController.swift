322a4be8e91a1165194af809f4f63746cefd5aa0
Planner0 <chishirik0@gmail.com>
2022-07-25 23:26:50 +0300

    feat: add Decodable

--- Netology_IB_Instruments/Modules/Main/Feed/ViewController/InfoViewController.swift
+++ Netology_IB_Instruments/Modules/Main/Feed/ViewController/InfoViewController.swift
@@ -8,19 +8,48 @@
 import UIKit
 
 class InfoViewController: UIViewController {
+    
+    let titleUILabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        dataTaskTwo(titleLabel: label)
+        return label
+    }()
+    
+    let orbitalPeriodUILabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        dataTask3(UILabel: label)
+        return label
+    }()
+    
     let alertButton = CustomButton(title: "press me now", color: .black)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .green
+        setup()
+        tapAlertButton()
+    }
+    
+    func setup(){
         self.view.addSubview(alertButton)
+        self.view.addSubview(titleUILabel)
+        self.view.addSubview(orbitalPeriodUILabel)
+        
         NSLayoutConstraint.activate([
             alertButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
             alertButton.heightAnchor.constraint(equalToConstant: 50),
             alertButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
-            alertButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50)
+            alertButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50),
+            titleUILabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            titleUILabel.bottomAnchor.constraint(equalTo: self.alertButton.topAnchor, constant: -50),
+            orbitalPeriodUILabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            orbitalPeriodUILabel.bottomAnchor.constraint(equalTo: self.titleUILabel.topAnchor, constant: -50)
+            
         ])
-        tapAlertButton()
     }
+    
     @objc func tapAlertButton(){
         alertButton.tapAction = { [weak self] in
             let alertVC = UIAlertController(title: "Error", message: "Fatal Error", preferredStyle: .alert)
