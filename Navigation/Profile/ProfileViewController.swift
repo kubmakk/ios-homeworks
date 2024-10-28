6d753cb2c0640187298b31e1662081bb68fc840a
kubmakk <rob1ox@ya.ru>
2024-10-29 01:58:03 +0300

    addFrameView

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -1,10 +1,20 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    private let profileHeaderView = ProfileHeaderController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .lightGray
         title = "Profile"
+        
+        view.addSubview(profileHeaderView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        profileHeaderView.frame = view.bounds
     }
 }
