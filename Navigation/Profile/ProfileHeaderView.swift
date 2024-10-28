a177cc5f1313a7026e4920e292d2838955b65046
kubmakk <rob1ox@ya.ru>
2024-10-29 02:22:39 +0300

    createAvatar

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -2,4 +2,30 @@ import UIKit
 
 class ProfileHeaderController: UIView {
     
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "avatar")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 50
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(avatarImageView)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        avatarImageView.frame = CGRect(x: 16, y: 16, width: 100, height: 100)
+        
+    }
+    
     }
