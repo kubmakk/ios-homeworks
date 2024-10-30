6c00fb94c465b3753e7bae932351d0bd5c247075
kubmakk <rob1ox@ya.ru>
2024-10-30 22:22:39 +0300

    createNameAndStatus

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,18 +9,34 @@ class ProfileHeaderController: UIView {
         imageView.clipsToBounds = false
         imageView.backgroundColor = .white
         imageView.layer.masksToBounds = true
-        
         imageView.layer.cornerRadius = 50
         imageView.layer.borderWidth = 3
         imageView.layer.borderColor = UIColor.black.cgColor
-        
         imageView.clipsToBounds = true
         return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "John Doe"
+        label.font = .systemFont(ofSize: .init(18), weight: .bold)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Online"
+        label.font = .systemFont(ofSize: .init(14), weight: .light)
+        label.textColor = .gray
+        return label
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(avatarImageView)
+        addSubview(nameLabel)
+        addSubview(statusLabel)
     }
     
     
@@ -30,7 +46,8 @@ class ProfileHeaderController: UIView {
     
     override func layoutSubviews() {
         avatarImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        
+        statusLabel.frame = CGRect(x: 0, y: 0, width: frame.width, height: 30)
+        nameLabel.frame = CGRect(x: 0, y: 0, width: frame.width, height: 30)
     }
     
     }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -12,7 +12,7 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .systemBackground
         title = "Profile"
         
         view.addSubview(profileHeaderView)
