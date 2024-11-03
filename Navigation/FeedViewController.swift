3fa07a442029589e2eb18b73137c90bbdacdcff7
kubmakk <rob1ox@ya.ru>
2024-11-04 02:54:47 +0300

    addStackView

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -4,8 +4,24 @@ class FeedViewController: UIViewController {
     
     lazy private var actionButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("Посмотреть пост", for: .normal)
+        button.setTitle("Посмотреть пост 1", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
+    }()
+    
+    lazy private var actionButton2: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Посмотреть пост 2", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [actionButton, actionButton2])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
     
     override func viewDidLoad() {
@@ -13,14 +29,19 @@ class FeedViewController: UIViewController {
         view.backgroundColor = .systemBackground
         title = "Feed"
         
-        actionButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
-        actionButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(actionButton)
+        view.addSubview(stackView)
         
+        actionButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
+        actionButton2.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            actionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            actionButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-            ])
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
     
     @objc func actionButtonTapped() {
