251626a02f76cd786b681f763f7f5899724c0787
kubmakk <rob1ox@ya.ru>
2024-11-08 02:29:44 +0300

    addButton

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -17,18 +17,76 @@ class LogInViewController: UIViewController {
     
     lazy private var loginField: UITextField = {
         let textField = UITextField()
-        textField.placeholder = "Login"
+        textField.backgroundColor = .systemGray6
+        textField.placeholder = "Email or phone"
+        textField.borderStyle = .roundedRect
         textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.layer.masksToBounds = true
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 10
+        //text
+        textField.textColor = .black
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.tintColor = UIColor(named: "Color")
+        textField.autocapitalizationType = .none
+
         return textField
+    }()
+    
+    lazy private var passField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .systemGray6
+        textField.placeholder = "Password"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.layer.masksToBounds = true
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 10
+        //text
+        textField.textColor = .black
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.tintColor = UIColor(named: "Color")
+        textField.autocapitalizationType = .none
+        textField.isSecureTextEntry = true
+
+        return textField
+    }()
+    
+    lazy private var loginButton: UIButton = {
+        let button = UIButton()
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        button.setBackgroundImage(UIImage(named: "blue_pixel"), for: .normal)
+        button.alpha = 1
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.layer.cornerRadius = 10
+
+        
+        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .white
         
         view.addSubview(logoIcon)
         view.addSubview(loginField)
+        view.addSubview(passField)
+        view.addSubview(loginButton)
+        
+        loginButton.addTarget(self, action: #selector(loginButtonTaped), for: .touchUpInside)
         setupConstraints()
+    }
+    
+    @objc func loginButtonTaped() {
+        let profileViewController = ProfileViewController()
+        navigationController?.pushViewController(profileViewController, animated: true)
     }
     
     private func setupConstraints() {
@@ -51,8 +109,30 @@ class LogInViewController: UIViewController {
                 constant: 16),
             loginField.trailingAnchor.constraint(
                 equalTo: safeAreaGuide.trailingAnchor,
-                constant: -16)
-            
+                constant: -16),
+            loginField.heightAnchor.constraint(equalToConstant: 50),
+            //Password Field
+            passField.heightAnchor.constraint(equalToConstant: 50),
+            passField.topAnchor.constraint(
+                equalTo: loginField.bottomAnchor,
+                constant: 0),
+            passField.leadingAnchor.constraint(
+                equalTo: safeAreaGuide.leadingAnchor,
+                constant: 16),
+            passField.trailingAnchor.constraint(
+                equalTo: safeAreaGuide.trailingAnchor,
+                constant: -16),
+            //Action Button
+            loginButton.leadingAnchor.constraint(
+                equalTo: safeAreaGuide.leadingAnchor,
+                constant: 16),
+            loginButton.trailingAnchor.constraint(
+                equalTo: safeAreaGuide.trailingAnchor,
+                constant: -16),
+            loginButton.topAnchor.constraint(
+                equalTo: passField.bottomAnchor,
+                constant: 16),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
         ])
 
     }
