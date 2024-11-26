56db617b20a38d90c8f3fff5d3c1c2535296316a
kubmakk <rob1ox@ya.ru>
2024-11-26 22:59:46 +0300

    addTableView

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -1,7 +1,21 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    // MARK: - Data
+    
+    fileprivate let data = Post.make()
+    
     // MARK: - Subview
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView.init(
+            frame: .zero,
+            style: .plain
+        )
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
     
     private var profileTableView: ProfileTableHeaderView = {
         let view = ProfileTableHeaderView()
@@ -9,17 +23,30 @@ class ProfileViewController: UIViewController {
         return view
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.backgroundColor = .lightGray
-        title = "Profile"
-        
-        view.addSubview(profileTableView)
-        
-        setupConstraints()
+    private enum CallReuseID: String {
+        case base = "BaseTableViewCell_ReuseID"
+        case custom = "CustomTableViewCell_ReuseID"
     }
     
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        setupView()
+        setupConstraints()
+    }
+    //MARK: - Private
+    private func setupView() {
+        view.backgroundColor = .lightGray
+        title = "Profile"
+        navigationItem.title = "Table"
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    
+    private func addSubviews() {
+        view.addSubview(profileTableView)
+        view.addSubview(tableView)
+    }
     private func setupConstraints() {
         let safeAreaGuide = view.safeAreaLayoutGuide
         
@@ -38,8 +65,18 @@ class ProfileViewController: UIViewController {
             ),
             profileTableView.heightAnchor.constraint(
                 equalToConstant: 220
-            )
+            ),
+            tableView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaGuide.bottomAnchor)
+            
             
         ])
+    }
+    
+    private func tuneTableView() {
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
     }
 }
