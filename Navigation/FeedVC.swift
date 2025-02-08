9a8b31538f9b82ef069c713bed416fb335f9e215
kubmakk <rob1ox@ya.ru>
2025-02-08 20:14:26 +0300

    addFeedModel

--- Navigation/FeedVC.swift
+++ Navigation/FeedVC.swift
@@ -6,14 +6,24 @@
 import UIKit
 import SnapKit
 final class FeedViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        view.backgroundColor = .systemTeal
-        
-        createSubView()
-    }
+    //MARK: Constraint
+    
+    private let secretWordField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Enter secret word"
+        return textField
+    }()
+    
+    private var checkGuessButton = CustomButton(
+        title: <#T##String#>,
+        titleColor: <#T##UIColor#>,
+        backroundColor: <#T##UIColor#>,
+        radius: <#T##CGFloat#>,
+        autoresizing: <#T##Bool#>,
+        target: <#T##AnyObject#>,
+        selector: <#T##Selector#>
+    )
     
     private func createSubView() {
         let stackView = UIStackView()
@@ -30,7 +40,6 @@ final class FeedViewController: UIViewController {
         ])
         addPostButton(title: "Post number One", color: .systemPurple, to: stackView, selector: #selector(tapPostButton))
         addPostButton(title: "Post number Two", color: .systemIndigo, to: stackView, selector: #selector(tapPostButton))
-        view.addSubview(button)
         
         button.snp.makeConstraints { make in
             make.top.equalTo(stackView.snp.bottom).offset(16)
@@ -50,7 +59,7 @@ final class FeedViewController: UIViewController {
     )
     
     private func addPostButton(title: String, color: UIColor, to view: UIStackView, selector: Selector) {
-        var button = CustomButton(
+        let button = CustomButton(
             title: title,
             titleColor: .white,
             backroundColor: color,
@@ -61,13 +70,33 @@ final class FeedViewController: UIViewController {
         )
         view.addArrangedSubview(button)
     }
+    //MARK: lifestyle
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    
+        view.backgroundColor = .systemTeal
+        view.addSubview(button)
+        createSubView()
+    }
+    //MARK: functions
     @objc func tapPostButton() {
         let post = postExamples[0]
         
         let postVC = PostViewController()
         postVC.post = post
         navigationController?.pushViewController(postVC, animated: true)
+    }
+}
+
+
+class FeedModel{
+    private let secretWord: String
+    
+    init(secretWord: String) {
+        self.secretWord
+    }
+    
+    func check(word: String) -> Bool {
+        word == self.secretWord
     }
 }
