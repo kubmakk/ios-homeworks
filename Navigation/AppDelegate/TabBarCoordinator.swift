6428874019ec2e63ecfc313c7081d525e9620a80
kubmakk <rob1ox@ya.ru>
2025-03-31 23:19:03 +0300

    addCoordinatorInTabBar

--- Navigation/AppDelegate/TabBarCoordinator.swift
+++ Navigation/AppDelegate/TabBarCoordinator.swift
@@ -12,7 +12,18 @@ final class TabBarCoordinator: BaseCoordinator {
     
     override func start() {
         let feedCoordinator = FeedCoordinator(navigationController: UINavigationController())
+        let profileCoordinator = ProfileCoordinator(navigationController: UINavigationController())
         
+        feedCoordinator.start()
+        profileCoordinator.start()
+        
+        tabBarController.viewControllers = [feedCoordinator.navigationController, profileCoordinator.navigationController]
+        
+        feedCoordinator.navigationController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "newspaper"), tag: 0)
+        profileCoordinator.navigationController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.circle"), tag: 1)
+        
+        navigationController.pushViewController(tabBarController, animated: false)
     }
+    
     
 }
