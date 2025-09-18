c6c67115e7d33ffaa85a7867fd07ebaa24535e3e
kubmakk <rob1ox@ya.ru>
2025-09-18 21:47:11 +0300

    addNewViewControllerInTabBat

--- Navigation/AppDelegate/TabBarCoordinator.swift
+++ Navigation/AppDelegate/TabBarCoordinator.swift
@@ -19,20 +19,25 @@ final class TabBarCoordinator: BaseCoordinator {
     override func start() {
         let feedNavigationController = UINavigationController()
         let profileNavigationController = UINavigationController()
+        let filesNavigationController = UINavigationController()
 
         let feedCoordinator = FeedCoordinator(navigationController: feedNavigationController)
         let loginCoordinator = LoginCoordinator(navigationController: profileNavigationController)
-
+        let filesCoordinator = FilesCoordinator(navigationController: filesNavigationController)
+        
         addChild(feedCoordinator)
         addChild(loginCoordinator)
+        addChild(filesCoordinator)
 
         feedCoordinator.start()
         loginCoordinator.start()
+        filesCoordinator.start()
 
         feedNavigationController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "newspaper"), tag: 0)
         profileNavigationController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.circle"), tag: 1)
+        filesNavigationController.tabBarItem = UITabBarItem(title: "Documents", image: UIImage(systemName: "folder"), tag: 2)
 
-        tabBarController.viewControllers = [feedNavigationController, profileNavigationController]
+        tabBarController.viewControllers = [feedNavigationController, profileNavigationController, filesNavigationController]
 
         rootNavigationController.setViewControllers([tabBarController], animated: false)
         rootNavigationController.isNavigationBarHidden = true 
---
+++ Navigation/Files/FilesVC.swift
@@ -0,0 +1,14 @@
+//
+//  FilesVC.swift
+//  Navigation
+//
+//  Created by kubmakk on 18/9/25.
+//
+import UIKit
+
+class filesViewController: UITableViewController {
+    weak var coordinator: FilesCoordinator?
+    
+    
+    
+}
