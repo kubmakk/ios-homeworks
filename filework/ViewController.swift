4b2c3422f5ade411d2bab1f26a91a5b7624df7bc
kubmakk <rob1ox@ya.ru>
2025-09-24 20:27:37 +0300

    Initial Commit

---
+++ filework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  filework
+//
+//  Created by kubmakk on 24/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
