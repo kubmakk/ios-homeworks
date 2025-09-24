299a9506c998379282de25bdc1b606fa67067ee4
kubmakk <rob1ox@ya.ru>
2025-09-24 21:12:29 +0300

    addCurrentStateUser

---
+++ filework/LoginVC.swift
@@ -0,0 +1,111 @@
+//
+//  LoginVC.swift
+//  filework
+//
+//  Created by kubmakk on 24/9/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    private enum State {
+        case CreatePass
+        case confirmPass(firstPass: String)
+        case login
+    }
+    
+    private var currentState: State = .login
+    
+    var success: (() -> Void)?
+    
+    private lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var passwordField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.isSecureTextEntry = true
+        field.borderStyle = .roundedRect
+        field.placeholder = "Минимум 4 символа"
+        field.textAlignment = .center
+        return field
+    }()
+    
+    private lazy var actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupUI()
+        updateUIfromState()
+    }
+    
+    private func updateUIfromState(){
+        passwordField.text = ""
+        
+        switch currentState {
+        case .CreatePass:
+            infoLabel.text = "Создайте пароль"
+            actionButton.setTitle("Создать", for: .normal)
+        case .confirmPass:
+            infoLabel.text = "Пароль должен совпадать"
+            actionButton.setTitle( "Повторить", for: .normal)
+        case .login:
+            infoLabel.text = "Пароль для входа"
+            actionButton.setTitle("Войти", for: .normal)
+        }
+    }
+    
+    private func setupUI() {
+            view.addSubview(infoLabel)
+            view.addSubview(passwordField)
+            view.addSubview(actionButton)
+            
+            NSLayoutConstraint.activate([
+                infoLabel.bottomAnchor.constraint(equalTo: passwordField.topAnchor, constant: -20),
+                infoLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+                infoLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+                
+                passwordField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                passwordField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+                passwordField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+                passwordField.heightAnchor.constraint(equalToConstant: 44),
+                
+                actionButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 20),
+                actionButton.leadingAnchor.constraint(equalTo: passwordField.leadingAnchor),
+                actionButton.trailingAnchor.constraint(equalTo: passwordField.trailingAnchor),
+                actionButton.heightAnchor.constraint(equalToConstant: 44)
+            ])
+        }
+    
+    private func handleInitialCreate(password: String) {
+        if password.count < 4 {
+            showAlert(title: "Ошибка", message: "Пароль должен содержать минимум 4 символа.")
+            return
+        }
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+    
+    @objc private func buttonTapped() {
+        
+    }
+}
+
---
+++ filework/PasswordManager.swift
@@ -0,0 +1,40 @@
+//
+//  PasswordManager.swift
+//  filework
+//
+//  Created by kubmakk on 24/9/25.
+//
+
+import Foundation
+import KeychainAccess
+
+class PasswordManager{
+    static var shared = PasswordManager()
+    
+    private let keychain = Keychain(service: "dasd")
+    private let passKey = "user_password"
+    
+    private init() {}
+    
+    var isPasswordSet: Bool {
+        return (try? keychain.get(passKey)) != nil
+    }
+    
+    func save(password: String) throws {
+        try keychain.set(password, key: passKey)
+    }
+    
+    func check(password: String) -> Bool {
+        guard let savedPassword = try? keychain.get(passKey) else {
+            return false
+        }
+        return password == savedPassword
+    }
+    
+    func deletePassword() throws {
+        try keychain.remove(passKey)
+    }
+    
+    
+    
+}
