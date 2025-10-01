ce81e5fb7ffc94ffbafdf2a43a1525aa6af7f539
kubmakk <rob1ox@ya.ru>
2025-10-01 16:22:26 +0300

    completeNetworkManager

--- myRealmProj/NetworkManager.swift
+++ myRealmProj/NetworkManager.swift
@@ -12,3 +12,31 @@ struct QuoteData: Decodable {
     let value: String
     let categories: [String]
 }
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private let url = URL(string: "https://api.chucknorris.io/jokes/random")!
+
+    private init() {}
+
+    func fetchQuote(completion: @escaping (Result<QuoteData, Error>) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(NSError(domain: "NetworkManager", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                return
+            }
+
+            do {
+                let quoteData = try JSONDecoder().decode(QuoteData.self, from: data)
+                completion(.success(quoteData))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
